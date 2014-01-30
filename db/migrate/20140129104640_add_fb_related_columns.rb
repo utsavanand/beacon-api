a14bbd7c6252c4729699ce05436eaef5059cd263
Utsav Anand <utsavanand.aggarwal@gmail.com>
2014-01-30 14:48:49 +0530

    removed un-necessary migration

--- db/migrate/20140129104640_add_fb_related_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddFbRelatedColumns < ActiveRecord::Migration
-  def change
-    add_column :users, :fb_token, :string
-    add_column :users, :fb_id, :string
-    add_column :users, :fb_token_expiry, :date
-  end
-end
