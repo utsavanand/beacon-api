55a131f906f186a8b69415c06c69bf34fdefacd4
Utsav Anand <utsavanand.aggarwal@gmail.com>
2014-02-13 14:49:38 +0530

    Change to the api for adding beacons

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -63,13 +63,14 @@ class ApiController < ApplicationController
   
   #To add an item to a beacon
   def addBeaconItemNote
-    if params[:type].present? && params[:item_id].present? && params[:note_title].present? && params[:note_type].present? && params[:note_image].present? && params[:note_thumbnail].present?
+    if params[:type].present? && params[:item_id].present? && params[:note_title].present? && params[:note_type].present? && params[:note_image].present? && params[:note_thumbnail].present? && params[:note_summary].present?
        if params[:type]=="addBeaconItemNote"
          @item = Item.find(params[:item_id])
          @note = Note.new
          @note.title = params[:note_title]
          @note.noteType = params[:note_type]
          @note.image = params[:note_image]
+         @note.summary = params[:note_summary]
          @note.thumbnail_image = params[:note_thumbnail]
          @note.item = @item
         if @note.save
